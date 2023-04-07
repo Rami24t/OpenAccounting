b00cfa24fe0e456027a4d88e09f1dd71fa85c8cb
RamS24 <AlSaadi.Rami24@gmail.com>
2023-04-07 19:05:43 +0200

    add auth middleware

---
+++ server/src/middlewares/authMiddleware.js
@@ -0,0 +1,45 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import User from "../models/User.js";
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+});
+
+const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an admin");
+  }
+};
+
+export { protect, admin, admin as isAdmin };
+// Using separate middleware functions for authentication and authorization provides more flexibility and makes the code more modular. If we have routes that only require authentication, we can use the protect middleware, and for routes that require both authentication and authorization, we can use protect followed by admin.
+
+//  adds an extra layer of security to ensure that only authorized users can access certain routes.
